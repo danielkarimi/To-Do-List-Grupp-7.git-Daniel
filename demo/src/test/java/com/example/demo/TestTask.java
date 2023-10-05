0d4fa79072ac27c943f2b028cf5bc54bbb203d0d
danielkarimi <daniel_karimii@outlook.com>
2023-10-05 18:18:48 +0200

    failtest

--- demo/src/test/java/com/example/demo/TestTask.java
+++ demo/src/test/java/com/example/demo/TestTask.java
@@ -2,42 +2,49 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
+
+
 public class TestTask {
 
   @Test
-  public void testGetId(){
+  public void testGetId() {
     Task task = new Task();
     task.setId(1L);
-    assert(task.getId() == 1L);
+    assert (task.getId() == 1L);
   }
 
   @Test
-  public void testGetDescription(){
+  public void testGetDescription() {
     Task task = new Task();
     task.setDescription("description");
-    assert(task.getDescription().equals("description"));
+    assert (task.getDescription().equals("description"));
   }
 
   @Test
-  public void testGetDeadline(){
+  public void testGetDeadline() {
     Task task = new Task();
     task.setDeadline("deadline");
-    assert(task.getDeadline().equals("deadline"));
+    assert (task.getDeadline().equals("deadline"));
   }
 
   @Test
-  public void testGetName(){
+  public void testGetName() {
     Task task = new Task();
     task.setName("name");
-    assert(task.getName().equals("name"));
+    assert (task.getName().equals("name"));
   }
 
   @Test
-  public void testGetStatus(){
+  public void testGetStatus() {
     Task task = new Task();
     task.setStatus(true);
-    assert(task.getStatus() == true);
+    assert (task.getStatus() == true);
   }
 
 
-}
+    @Test
+    public void testFailure() {
+      assert(false);
+    }
+  }
+
